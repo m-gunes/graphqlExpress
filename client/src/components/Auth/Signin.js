1ac4af816451be5088f24a58bd1de9fc5a0fb136
mgunes <mistafagunes@gmail.com>
2018-09-22 15:30:44 +0300

    implement signin mutation on Client

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { SIGNIN_USER } from '../../queries';
+import Error from '../Error';
+
+const initialState = {
+   username: '',
+   password: ''
+}
 
 class Signin extends Component {
+   state = { ...initialState }
+
+   clearState = () => {
+      this.setState({ ...initialState })
+   }
+
+   validateForm = () => {
+      const {username, password} = this.state;
+      const isValid = !username || !password;
+      return isValid;
+   }
+
+   handleChange = (event) => {
+      const {name, value} = event.target;
+      this.setState({[name]: value})
+   }
+   handleSubmit = (event, signinUser) => {
+      event.preventDefault();
+      signinUser().then(data => {
+         console.log(data)
+         this.clearState();
+      })
+   }
    render() {
+      const {username, password} = this.state;
       return (
-         <div> Signin
+         <div className='App'>
+            <h2>Signin</h2>
+            <Mutation mutation={SIGNIN_USER} variables={{username, password}}>
+               {(signinUser, {data, loading, error}) => {
+                  
+                  return (
+                     <form className="form" onSubmit={event => this.handleSubmit(event, signinUser)}>
+                        <input type="text" name='username' placeholder='Username' value={username} onChange={this.handleChange}/>
+                        <input type="password" name='password' placeholder='Password' value={password} onChange={this.handleChange} />
+                        <button type='submit' disabled={loading || this.validateForm() } className='button-primary'>Submit</button>
+                        {error && <Error error={error}/> }
+                     </form>
+                  )
+               }}
+            </Mutation>
             
          </div>
       );
